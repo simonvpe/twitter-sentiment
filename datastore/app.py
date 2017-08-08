342c11427b9684c58b61bf6ab0a07126b2413c02
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-08-08 12:21:34 +0200

    Removed rate limiting from datastore (it will be moved to proxy)

--- datastore/app.py
+++ datastore/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, Response, request
 from flask_api import FlaskAPI, exceptions
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 
 import os, datetime
@@ -10,17 +8,10 @@ import os, datetime
 app = FlaskAPI(__name__)
 
 DB_PATH  = os.environ['DB_PATH']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath(DB_PATH)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath(DB_PATH) + 'db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db  = SQLAlchemy(app)
-
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"]
-)
-
 
 class Sentiment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +24,7 @@ class Sentiment(db.Model):
         self.value = value
 
 @app.route("/api/<string:table>", methods=["GET", "POST"], strict_slashes=False)
+
 def list(table):
     if request.method == "GET":
         records = Sentiment.query.filter_by(key=table).all()
